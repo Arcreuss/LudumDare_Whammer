b68240ec272284b4c3d07a027916d97c5f5dad21
GuillaumeClerc <clercguigui62@gmail.com>
2023-09-30 01:00:32 -0400

    Add BonusMoveSpeed
    Add Bonus that up Move Speed Player

--- StarterTemplate/Source/StarterTemplate/Private/Bonus/BonusMoveSpeed.cpp
+++ StarterTemplate/Source/StarterTemplate/Private/Bonus/BonusMoveSpeed.cpp
@@ -1,19 +1,20 @@
 ﻿// UnrealEngine Starter template - by TechNet 2023
 
 
-#include "Bonus/BonusAttackSpeed.h"
+#include "Bonus/BonusMoveSpeed.h"
 
 
 // Sets default values
-ABonusAttackSpeed::ABonusAttackSpeed()
+ABonusMoveSpeed::ABonusMoveSpeed()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 }
 
 // Called when the game starts or when spawned
-void ABonusAttackSpeed::BeginPlay()
+void ABonusMoveSpeed::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
+
