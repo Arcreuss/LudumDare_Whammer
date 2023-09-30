70f068c136ff1ef5b78903713a4c3a85254a6cf4
GuillaumeClerc <clercguigui62@gmail.com>
2023-09-30 18:31:06 -0400

    Add Cracked wall to Room

--- StarterTemplate/Source/StarterTemplate/Private/LevelActor/Room.cpp
+++ StarterTemplate/Source/StarterTemplate/Private/LevelActor/Room.cpp
@@ -7,7 +7,7 @@
 ARoom::ARoom()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 	
 	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BoxMesh"));
 	BaseMesh->SetupAttachment(RootComponent);
@@ -22,11 +22,7 @@ UStaticMeshComponent* ARoom::GetBoxMesh() const
 // Called when the game starts or when spawned
 void ARoom::BeginPlay()
 {
-	Super::BeginPlay();
-
-	const int32 RandomIndex = FMath::RandRange(0, 2);
-	const int32 RandomRotation = FMath::RandRange(0, 3);
-	
+	Super::BeginPlay();	
 	
 	if (Meshes[RandomIndex])
 		BaseMesh->SetStaticMesh(Meshes[RandomIndex]);
@@ -48,11 +44,5 @@ void ARoom::BeginPlay()
 			BaseMesh->SetRelativeRotation(FRotator(0, 270, 0));
 			break;
 		}
-}
-
-// Called every frame
-void ARoom::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
 }
 
